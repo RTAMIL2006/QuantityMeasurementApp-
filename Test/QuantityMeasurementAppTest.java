b269394383995a9ff9087d7156e9b18b43761e14
RTAMIL2006 <tamilselvan28762006@gmail.com>
2026-05-05 10:24:13 +0530

    UC813:Centralized Arithmetic Logic to Enforce DRY in Quantity Operations

--- Test/QuantityMeasurementAppTest.java
+++ Test/QuantityMeasurementAppTest.java
@@ -1,147 +1,175 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class QuantityUC12Test {
+class QuantityUC13Test {
 
     private static final double EPS = 1e-6;
 
-    @Test
-    void testSubtraction_SameUnit_Length() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var r = a.subtract(b);
-        assertEquals(5.0, r.getValue(), EPS);
-    }
+    // ---------- ADD (Behavior preserved) ----------
 
     @Test
-    void testSubtraction_CrossUnit_Length() {
+    void testAdd_BehaviorPreserved() {
+        var a = new QuantityMeasurementApp.Quantity<>(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        var b = new QuantityMeasurementApp.Quantity<>(12.0, QuantityMeasurementApp.LengthUnit.INCHES);
+
+        var r = a.add(b);
+        assertEquals(2.0, r.getValue(), EPS);
+    }
+
+    // ---------- SUBTRACT ----------
+
+    @Test
+    void testSubtract_DelegationAndResult() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
         var b = new QuantityMeasurementApp.Quantity<>(6.0, QuantityMeasurementApp.LengthUnit.INCHES);
+
         var r = a.subtract(b);
         assertEquals(9.5, r.getValue(), EPS);
     }
 
     @Test
-    void testSubtraction_ExplicitTarget_Length() {
+    void testSubtract_ExplicitTargetUnit() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
         var b = new QuantityMeasurementApp.Quantity<>(6.0, QuantityMeasurementApp.LengthUnit.INCHES);
+
         var r = a.subtract(b, QuantityMeasurementApp.LengthUnit.INCHES);
         assertEquals(114.0, r.getValue(), EPS);
     }
 
-    @Test
-    void testSubtraction_NegativeResult() {
-        var a = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var r = a.subtract(b);
-        assertEquals(-5.0, r.getValue(), EPS);
-    }
+    // ---------- DIVIDE ----------
 
     @Test
-    void testSubtraction_ZeroResult() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(120.0, QuantityMeasurementApp.LengthUnit.INCHES);
-        var r = a.subtract(b);
-        assertEquals(0.0, r.getValue(), EPS);
-    }
-
-    @Test
-    void testSubtraction_Weight() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.WeightUnit.KILOGRAM);
-        var b = new QuantityMeasurementApp.Quantity<>(5000.0, QuantityMeasurementApp.WeightUnit.GRAM);
-        var r = a.subtract(b);
-        assertEquals(5.0, r.getValue(), EPS);
-    }
-
-    @Test
-    void testSubtraction_Volume() {
-        var a = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.VolumeUnit.LITRE);
-        var b = new QuantityMeasurementApp.Quantity<>(500.0, QuantityMeasurementApp.VolumeUnit.MILLILITRE);
-        var r = a.subtract(b);
-        assertEquals(4.5, r.getValue(), EPS);
-    }
-
-    @Test
-    void testSubtraction_NullOperand() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        assertThrows(IllegalArgumentException.class, () -> a.subtract(null));
-    }
-
-    @Test
-    void testSubtraction_CrossCategory() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.WeightUnit.KILOGRAM);
-        assertThrows(IllegalArgumentException.class, () -> a.subtract((QuantityMeasurementApp.Quantity) b));
-    }
-
-    @Test
-    void testDivision_SameUnit_Length() {
+    void testDivide_DelegationAndResult() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
         var b = new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.LengthUnit.FEET);
+
         assertEquals(5.0, a.divide(b), EPS);
     }
 
     @Test
-    void testDivision_CrossUnit_Length() {
+    void testDivide_CrossUnit() {
         var a = new QuantityMeasurementApp.Quantity<>(24.0, QuantityMeasurementApp.LengthUnit.INCHES);
         var b = new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.LengthUnit.FEET);
+
         assertEquals(1.0, a.divide(b), EPS);
     }
 
-    @Test
-    void testDivision_RatioLessThanOne() {
-        var a = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        assertEquals(0.5, a.divide(b), EPS);
-    }
+    // ---------- VALIDATION (Centralized) ----------
 
     @Test
-    void testDivision_Weight() {
-        var a = new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.WeightUnit.KILOGRAM);
-        var b = new QuantityMeasurementApp.Quantity<>(2000.0, QuantityMeasurementApp.WeightUnit.GRAM);
-        assertEquals(1.0, a.divide(b), EPS);
-    }
-
-    @Test
-    void testDivision_Volume() {
-        var a = new QuantityMeasurementApp.Quantity<>(1000.0, QuantityMeasurementApp.VolumeUnit.MILLILITRE);
-        var b = new QuantityMeasurementApp.Quantity<>(1.0, QuantityMeasurementApp.VolumeUnit.LITRE);
-        assertEquals(1.0, a.divide(b), EPS);
-    }
-
-    @Test
-    void testDivision_ByZero() {
+    void testValidation_NullOperand_AllOperations() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(0.0, QuantityMeasurementApp.LengthUnit.FEET);
-        assertThrows(ArithmeticException.class, () -> a.divide(b));
-    }
 
-    @Test
-    void testDivision_NullOperand() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertThrows(IllegalArgumentException.class, () -> a.add(null));
+        assertThrows(IllegalArgumentException.class, () -> a.subtract(null));
         assertThrows(IllegalArgumentException.class, () -> a.divide(null));
     }
 
     @Test
-    void testDivision_CrossCategory() {
-        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.WeightUnit.KILOGRAM);
-        assertThrows(IllegalArgumentException.class, () -> a.divide((QuantityMeasurementApp.Quantity) b));
+    void testValidation_CrossCategory_AllOperations() {
+        var length = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
+        var weight = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.WeightUnit.KILOGRAM);
+
+        assertThrows(IllegalArgumentException.class, () -> length.add((QuantityMeasurementApp.Quantity) weight));
+        assertThrows(IllegalArgumentException.class, () -> length.subtract((QuantityMeasurementApp.Quantity) weight));
+        assertThrows(IllegalArgumentException.class, () -> length.divide((QuantityMeasurementApp.Quantity) weight));
     }
 
     @Test
-    void testIntegration_AddSubtractInverse() {
+    void testValidation_NullTargetUnit() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var r = a.add(b).subtract(b);
-        assertEquals(a.getValue(), r.getValue(), EPS);
+        var b = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.LengthUnit.FEET);
+
+        assertThrows(IllegalArgumentException.class, () -> a.add(b, null));
+        assertThrows(IllegalArgumentException.class, () -> a.subtract(b, null));
+    }
+
+    // ---------- ENUM OPERATION TEST ----------
+
+    @Test
+    void testArithmeticEnum_ADD() {
+        double result = QuantityMeasurementApp.ArithmeticOperation.ADD.compute(10, 5);
+        assertEquals(15.0, result, EPS);
     }
 
     @Test
-    void testChainedSubtraction() {
+    void testArithmeticEnum_SUBTRACT() {
+        double result = QuantityMeasurementApp.ArithmeticOperation.SUBTRACT.compute(10, 5);
+        assertEquals(5.0, result, EPS);
+    }
+
+    @Test
+    void testArithmeticEnum_DIVIDE() {
+        double result = QuantityMeasurementApp.ArithmeticOperation.DIVIDE.compute(10, 5);
+        assertEquals(2.0, result, EPS);
+    }
+
+    @Test
+    void testArithmeticEnum_DivideByZero() {
+        assertThrows(ArithmeticException.class,
+                () -> QuantityMeasurementApp.ArithmeticOperation.DIVIDE.compute(10, 0));
+    }
+
+    // ---------- ROUNDING ----------
+
+    @Test
+    void testRounding_AddSubtract() {
+        var a = new QuantityMeasurementApp.Quantity<>(1.234, QuantityMeasurementApp.LengthUnit.FEET);
+        var b = new QuantityMeasurementApp.Quantity<>(0.001, QuantityMeasurementApp.LengthUnit.FEET);
+
+        var r = a.add(b);
+        assertEquals(1.24, r.getValue(), EPS);
+    }
+
+    @Test
+    void testDivide_NoRounding() {
         var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var r = a.subtract(new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.LengthUnit.FEET))
+        var b = new QuantityMeasurementApp.Quantity<>(3.0, QuantityMeasurementApp.LengthUnit.FEET);
+
+        assertEquals(10.0 / 3.0, a.divide(b), EPS);
+    }
+
+    // ---------- IMMUTABILITY ----------
+
+    @Test
+    void testImmutability_AfterOperations() {
+        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
+        var b = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.LengthUnit.FEET);
+
+        a.add(b);
+        a.subtract(b);
+        a.divide(b);
+
+        assertEquals(10.0, a.getValue(), EPS);
+    }
+
+    // ---------- CHAIN OPERATIONS ----------
+
+    @Test
+    void testChainedOperations() {
+        var a = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET);
+
+        var result = a.add(new QuantityMeasurementApp.Quantity<>(2.0, QuantityMeasurementApp.LengthUnit.FEET))
                 .subtract(new QuantityMeasurementApp.Quantity<>(1.0, QuantityMeasurementApp.LengthUnit.FEET));
-        assertEquals(7.0, r.getValue(), EPS);
+
+        assertEquals(11.0, result.getValue(), EPS);
+    }
+
+    // ---------- MULTI CATEGORY ----------
+
+    @Test
+    void testAllCategories_Working() {
+
+        var length = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.LengthUnit.FEET)
+                .subtract(new QuantityMeasurementApp.Quantity<>(6.0, QuantityMeasurementApp.LengthUnit.INCHES));
+        assertEquals(9.5, length.getValue(), EPS);
+
+        var weight = new QuantityMeasurementApp.Quantity<>(10.0, QuantityMeasurementApp.WeightUnit.KILOGRAM)
+                .add(new QuantityMeasurementApp.Quantity<>(1000.0, QuantityMeasurementApp.WeightUnit.GRAM));
+        assertEquals(11.0, weight.getValue(), EPS);
+
+        var volume = new QuantityMeasurementApp.Quantity<>(5.0, QuantityMeasurementApp.VolumeUnit.LITRE)
+                .subtract(new QuantityMeasurementApp.Quantity<>(500.0, QuantityMeasurementApp.VolumeUnit.MILLILITRE));
+        assertEquals(4.5, volume.getValue(), EPS);
     }
 }
--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,5 +1,9 @@
+import java.util.Objects;
+import java.util.function.DoubleBinaryOperator;
+
 public class QuantityMeasurementApp {
 
+    // ---------------- INTERFACE ----------------
     public interface IMeasurable {
         double getConversionFactor();
         double convertToBaseUnit(double value);
@@ -7,6 +11,7 @@ public class QuantityMeasurementApp {
         String getUnitName();
     }
 
+    // ---------------- LENGTH ----------------
     public enum LengthUnit implements IMeasurable {
         FEET(1.0),
         INCHES(1.0 / 12.0),
@@ -19,23 +24,13 @@ public class QuantityMeasurementApp {
             this.factor = factor;
         }
 
-        public double getConversionFactor() {
-            return factor;
-        }
-
-        public double convertToBaseUnit(double value) {
-            return value * factor;
-        }
-
-        public double convertFromBaseUnit(double baseValue) {
-            return baseValue / factor;
-        }
-
-        public String getUnitName() {
-            return name();
-        }
+        public double getConversionFactor() { return factor; }
+        public double convertToBaseUnit(double value) { return value * factor; }
+        public double convertFromBaseUnit(double baseValue) { return baseValue / factor; }
+        public String getUnitName() { return name(); }
     }
 
+    // ---------------- WEIGHT ----------------
     public enum WeightUnit implements IMeasurable {
         KILOGRAM(1.0),
         GRAM(0.001),
@@ -47,23 +42,13 @@ public class QuantityMeasurementApp {
             this.factor = factor;
         }
 
-        public double getConversionFactor() {
-            return factor;
-        }
-
-        public double convertToBaseUnit(double value) {
-            return value * factor;
-        }
-
-        public double convertFromBaseUnit(double baseValue) {
-            return baseValue / factor;
-        }
-
-        public String getUnitName() {
-            return name();
-        }
+        public double getConversionFactor() { return factor; }
+        public double convertToBaseUnit(double value) { return value * factor; }
+        public double convertFromBaseUnit(double baseValue) { return baseValue / factor; }
+        public String getUnitName() { return name(); }
     }
 
+    // ---------------- VOLUME ----------------
     public enum VolumeUnit implements IMeasurable {
         LITRE(1.0),
         MILLILITRE(0.001),
@@ -75,126 +60,153 @@ public class QuantityMeasurementApp {
             this.factor = factor;
         }
 
-        public double getConversionFactor() {
-            return factor;
+        public double getConversionFactor() { return factor; }
+        public double convertToBaseUnit(double value) { return value * factor; }
+        public double convertFromBaseUnit(double baseValue) { return baseValue / factor; }
+        public String getUnitName() { return name(); }
+    }
+
+    // ---------------- ARITHMETIC ENUM ----------------
+    public enum ArithmeticOperation {
+        ADD((a, b) -> a + b),
+        SUBTRACT((a, b) -> a - b),
+        DIVIDE((a, b) -> {
+            if (b == 0) throw new ArithmeticException("Division by zero");
+            return a / b;
+        });
+
+        private final DoubleBinaryOperator op;
+
+        ArithmeticOperation(DoubleBinaryOperator op) {
+            this.op = op;
         }
 
-        public double convertToBaseUnit(double value) {
-            return value * factor;
-        }
-
-        public double convertFromBaseUnit(double baseValue) {
-            return baseValue / factor;
-        }
-
-        public String getUnitName() {
-            return name();
+        public double compute(double a, double b) {
+            return op.applyAsDouble(a, b);
         }
     }
 
+    // ---------------- GENERIC QUANTITY ----------------
     public static class Quantity<U extends IMeasurable> {
         private final double value;
         private final U unit;
-        private static final double EPSILON = 1e-6;
+        private static final double EPS = 1e-6;
 
         public Quantity(double value, U unit) {
-            if (unit == null || !Double.isFinite(value)) throw new IllegalArgumentException();
+            if (unit == null) throw new IllegalArgumentException("Unit cannot be null");
+            if (!Double.isFinite(value)) throw new IllegalArgumentException("Invalid value");
             this.value = value;
             this.unit = unit;
         }
 
-        public double getValue() {
-            return value;
+        public double getValue() { return value; }
+        public U getUnit() { return unit; }
+
+        // ---------- VALIDATION ----------
+        private void validate(Quantity<U> other, U target, boolean checkTarget) {
+            if (other == null) throw new IllegalArgumentException("Other cannot be null");
+            if (unit.getClass() != other.unit.getClass())
+                throw new IllegalArgumentException("Different categories");
+            if (!Double.isFinite(other.value))
+                throw new IllegalArgumentException("Invalid value");
+
+            if (checkTarget && target == null)
+                throw new IllegalArgumentException("Target unit cannot be null");
         }
 
-        public U getUnit() {
-            return unit;
+        // ---------- CORE HELPER ----------
+        private double performBaseArithmetic(Quantity<U> other, ArithmeticOperation op) {
+            double base1 = unit.convertToBaseUnit(value);
+            double base2 = other.unit.convertToBaseUnit(other.value);
+            return op.compute(base1, base2);
         }
 
-        private double toBase() {
-            return unit.convertToBaseUnit(value);
+        private double round(double val) {
+            return Math.round(val * 100.0) / 100.0;
         }
 
-        private double round(double v) {
-            return Math.round(v * 100.0) / 100.0;
-        }
-
-        public Quantity<U> convertTo(U target) {
-            if (target == null) throw new IllegalArgumentException();
-            double result = target.convertFromBaseUnit(toBase());
-            return new Quantity<>(round(result), target);
-        }
-
+        // ---------- ADD ----------
         public Quantity<U> add(Quantity<U> other) {
-            return add(other, this.unit);
+            return add(other, unit);
         }
 
         public Quantity<U> add(Quantity<U> other, U target) {
-            validate(other, target);
-            double sum = this.toBase() + other.toBase();
-            return new Quantity<>(round(target.convertFromBaseUnit(sum)), target);
+            validate(other, target, true);
+            double base = performBaseArithmetic(other, ArithmeticOperation.ADD);
+            return new Quantity<>(round(target.convertFromBaseUnit(base)), target);
         }
 
+        // ---------- SUBTRACT ----------
         public Quantity<U> subtract(Quantity<U> other) {
-            return subtract(other, this.unit);
+            return subtract(other, unit);
         }
 
         public Quantity<U> subtract(Quantity<U> other, U target) {
-            validate(other, target);
-            double diff = this.toBase() - other.toBase();
-            return new Quantity<>(round(target.convertFromBaseUnit(diff)), target);
+            validate(other, target, true);
+            double base = performBaseArithmetic(other, ArithmeticOperation.SUBTRACT);
+            return new Quantity<>(round(target.convertFromBaseUnit(base)), target);
         }
 
+        // ---------- DIVIDE ----------
         public double divide(Quantity<U> other) {
-            if (other == null) throw new IllegalArgumentException();
-            if (!unit.getClass().equals(other.unit.getClass())) throw new IllegalArgumentException();
-            double divisor = other.toBase();
-            if (Math.abs(divisor) < EPSILON) throw new ArithmeticException();
-            return this.toBase() / divisor;
+            validate(other, null, false);
+            return performBaseArithmetic(other, ArithmeticOperation.DIVIDE);
         }
 
-        private void validate(Quantity<U> other, U target) {
-            if (other == null || target == null) throw new IllegalArgumentException();
-            if (!unit.getClass().equals(other.unit.getClass())) throw new IllegalArgumentException();
+        // ---------- CONVERT ----------
+        public Quantity<U> convertTo(U target) {
+            if (target == null) throw new IllegalArgumentException("Target null");
+            double base = unit.convertToBaseUnit(value);
+            return new Quantity<>(round(target.convertFromBaseUnit(base)), target);
         }
 
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Quantity<?> other = (Quantity<?>) obj;
-            if (!unit.getClass().equals(other.unit.getClass())) return false;
-            return Math.abs(this.toBase() - other.toBase()) < EPSILON;
+        // ---------- EQUALS ----------
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Quantity<?> q)) return false;
+            if (unit.getClass() != q.unit.getClass()) return false;
+
+            double b1 = unit.convertToBaseUnit(value);
+            double b2 = q.unit.convertToBaseUnit(q.value);
+
+            return Math.abs(b1 - b2) < EPS;
         }
 
+        @Override
         public int hashCode() {
-            return Double.hashCode(round(toBase()));
+            return Objects.hash(unit.convertToBaseUnit(value));
         }
 
+        @Override
         public String toString() {
             return "Quantity(" + value + ", " + unit.getUnitName() + ")";
         }
     }
 
+    // ---------------- MAIN METHOD ----------------
     public static void main(String[] args) {
 
-        Quantity<LengthUnit> l1 = new Quantity<>(10.0, LengthUnit.FEET);
-        Quantity<LengthUnit> l2 = new Quantity<>(6.0, LengthUnit.INCHES);
+        // LENGTH
+        var l1 = new Quantity<>(10.0, LengthUnit.FEET);
+        var l2 = new Quantity<>(6.0, LengthUnit.INCHES);
 
-        System.out.println(l1.subtract(l2));
-        System.out.println(l1.subtract(l2, LengthUnit.INCHES));
-        System.out.println(l1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
+        System.out.println("Add: " + l1.add(l2));
+        System.out.println("Subtract: " + l1.subtract(l2));
+        System.out.println("Divide: " + l1.divide(l2));
 
-        Quantity<WeightUnit> w1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
-        Quantity<WeightUnit> w2 = new Quantity<>(5000.0, WeightUnit.GRAM);
+        // WEIGHT
+        var w1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
+        var w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
 
-        System.out.println(w1.subtract(w2));
-        System.out.println(w1.divide(new Quantity<>(5.0, WeightUnit.KILOGRAM)));
+        System.out.println("Weight Add: " + w1.add(w2));
+        System.out.println("Weight Subtract: " + w1.subtract(w2));
 
-        Quantity<VolumeUnit> v1 = new Quantity<>(5.0, VolumeUnit.LITRE);
-        Quantity<VolumeUnit> v2 = new Quantity<>(500.0, VolumeUnit.MILLILITRE);
+        // VOLUME
+        var v1 = new Quantity<>(5.0, VolumeUnit.LITRE);
+        var v2 = new Quantity<>(500.0, VolumeUnit.MILLILITRE);
 
-        System.out.println(v1.subtract(v2));
-        System.out.println(v1.subtract(v2, VolumeUnit.MILLILITRE));
-        System.out.println(v1.divide(new Quantity<>(10.0, VolumeUnit.LITRE)));
+        System.out.println("Volume Add: " + v1.add(v2));
+        System.out.println("Volume Subtract: " + v1.subtract(v2));
     }
 }
